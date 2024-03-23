810e0e16bfea93aef0cd47b701ae27ed3ad8a2bb
Shmily <144188875+19shmily@users.noreply.github.com>
2024-03-23 21:21:38 +0800

    Add files via upload

---
+++ 189.轮转数组.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+class Solution_189 {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = k % nums.size();
+        if (n > (nums.size() / 2))
+        {
+            n = nums.size() - n;
+            int tmp = nums.size();
+            nums.resize(nums.size() + n);
+            copy(nums.begin(), nums.begin() + n, nums.begin() + tmp);
+            nums.erase(nums.begin(), nums.begin() + n);
+            return;
+        }
+        for (int m = 0; m < n; m++)
+        {
+            nums.insert(nums.begin(), nums.back());
+            nums.pop_back();
+        }
+        return;
+    }
+};
+
+int main()
+{
+    Solution_189 test;
+    vector<int>nums;
+    nums.push_back(1);
+    nums.push_back(2);
+    nums.push_back(3);
+    nums.push_back(4);
+    nums.push_back(5);
+    nums.push_back(6);
+
+    test.rotate(nums, 11);
+    return 0;
+}
